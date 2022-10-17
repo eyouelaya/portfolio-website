2dabf668bef8bf9604c67b79d133d8d81a44226b
Eyouel <eyouel@DESKTOP-B7KP3AS.localdomain>
2022-10-18 00:03:17 +0100

    header and socials complete

--- src/component/header/Header.jsx
+++ src/component/header/Header.jsx
@@ -1,7 +1,25 @@
 import React from 'react'
 import './header.css'
+import {CTA} from './CTA'
+import {Socials} from './Socials'
+import ME from '../../assets/me.png'
+
 export const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <div className="container header__container">
+        <h5>Hello I'm</h5>
+        <h1>Eyouel Ayalew Kenfu</h1>
+        <h5 className="text-light">Full Stack Developer</h5>
+        <CTA />
+       
+        <div className="me">
+          <img src={ME} alt="me" />
+        </div>
+
+        <a href="#contact" className='scroll__down'>Scroll Down</a>
+        <Socials />
+      </div>
+    </header>
   )
 }
