c2fabb11b1609bfcc9b3df23f0a4aff7a68c6cb6
Eyouel <eyouel@DESKTOP-B7KP3AS.localdomain>
2022-10-29 15:56:53 +0100

    about page text

--- src/component/about/About.jsx
+++ src/component/about/About.jsx
@@ -1,9 +1,6 @@
 import React from 'react'
 import './about.css'
-import ME from '../../assets/me-about.jpg'
-import {FaAward} from 'react-icons/fa'
-import {CgOrganisation} from 'react-icons/cg'
-import {VscLibrary} from 'react-icons/vsc'
+import ME from '../../assets/me-about.jpg' 
 
 export const About = () => {
   return (
@@ -18,27 +15,7 @@ export const About = () => {
         </div>
       </div>
       <div className="about__content">
-          <div className="about__cards">
-           
-            <article className='about__card'>
-              < FaAward className='about__icon'/>
-              <h5>Experience</h5>
-              <small>3+ Years Working</small>
-            </article>
-           
-            <article className='about__card'>
-              < CgOrganisation className='about__icon'/>
-              <h5>Experience</h5>
-              <small>2+ Organisation</small>
-            </article>
-            
-            <article className='about__card'>
-              < VscLibrary className='about__icon'/>
-              <h5>Projects</h5>
-              <small>80+ Projects</small>
-            </article>
-          </div>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto sequi debitis vero hic fugiat, odit, minus magni suscipit dolorum earum, exercitationem non ex amet ipsa numquam velit corporis voluptatum. Quam.</p>
+          <p>Currently a Market Specialist at META(FB), I support the Sub-Saharan Market by initiating and leading projects to ensure the safety of our communities. I work with multiple cross-functional teams within the Risk Prevention team. Through my experience in various roles throughout my career, I have developed strong problem-solving, critical thinking, software development, partner management, and leadership skills.</p>
         <a href='#contact' className='btn btn-primary'>Let's Talk</a>
         </div>
       </div>
