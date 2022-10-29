beebe079f7a5e8d311e370999327c325279b6fc5
Eyouel <eyouel@DESKTOP-B7KP3AS.localdomain>
2022-10-29 17:06:03 +0100

    experience

--- src/component/experience/Experience.jsx
+++ src/component/experience/Experience.jsx
@@ -11,86 +11,103 @@ export const Experience = () => {
             <h3>Skills</h3>
             <div className="experience__content">
               <article className='experience__details'>
-                <BsCheck2Square className='experience__details-icon' />
-                <div>
+                <div className='experience__details-div'>
+                  <BsCheck2Square className='experience__details-icon' />
                   <h6>Project Management</h6>
-                  <small className='text-light'> </small>
                 </div>
               </article>
 
               <article className='experience__details'>
-                <BsCheck2Square className='experience__details-icon' />
-                <div>
+                <div className='experience__details-div'>
+                  <BsCheck2Square className='experience__details-icon' />
                   <h6>Problem Solving</h6>
-                  <small className='text-light'> experienced</small>
                 </div>
               </article>
 
               <article className='experience__details'>
-                <BsCheck2Square className='experience__details-icon' />
-                <div>
+                <div className='experience__details-div'>
+                  <BsCheck2Square className='experience__details-icon' />
                   <h6>Stakeholder Managment</h6>
-                  <small className='text-light'> experienced</small>
                 </div>
               </article>
 
               <article className='experience__details'>
-                <BsCheck2Square className='experience__details-icon' />
-                <div>
-                  <h6>Self Starter</h6>
-                  <small className='text-light'> experienced</small>
+                <div className='experience__details-div'>
+                  <BsCheck2Square className='experience__details-icon' />
+                  <h6>Critical Thinking</h6>
                 </div>
               </article>
 
               <article className='experience__details'>
-                <BsCheck2Square className='experience__details-icon' />
-                <div>
-                  <h6>Problem Solving</h6>
-                  <small className='text-light'> experienced</small>
+                <div className='experience__details-div'>
+                  <BsCheck2Square className='experience__details-icon' />
+                  <h6>Leadership</h6>
+                </div>
+              </article>
+
+              <article className='experience__details'>
+                <div className='experience__details-div'>
+                  <BsCheck2Square className='experience__details-icon' />
+                  <h6>Software Development</h6>
                 </div>
               </article>
             </div>
         </div>
         <div className="experience__tools">
-        <h3>Programming Lanaguages/Tools</h3>
+        <h3>Languages/Frameworks</h3>
             <div className="experience__content">
               <article className='experience__details'>
-                <BsCheck2Square className='experience__details-icon' />
-                <div>
+                <div className='experience__details-div'>
+                  <BsCheck2Square className='experience__details-icon' />
                   <h6>Javascript</h6>
-                  <small className='text-light'> experienced</small>
                 </div>
               </article>
 
               <article className='experience__details'>
-                <BsCheck2Square className='experience__details-icon' />
-                <div>
+                <div className='experience__details-div'>
+                  <BsCheck2Square className='experience__details-icon' />
                   <h6>MySQL</h6>
-                  <small className='text-light'> experienced</small>
                 </div>
               </article>
 
               <article className='experience__details'>
-                <BsCheck2Square className='experience__details-icon' />
-                <div>
+                <div className='experience__details-div'>
+                  <BsCheck2Square className='experience__details-icon' />
+                  <h6>React JS</h6>
+                </div>
+                </article>
+
+              <article className='experience__details'>
+                <div className='experience__details-div'>
+                  <BsCheck2Square className='experience__details-icon' />
+                  <h6>Laravel</h6>
+                </div>
+                </article>
+
+              <article className='experience__details'>
+                <div className='experience__details-div'>
+                  <BsCheck2Square className='experience__details-icon' />
+                  <h6>Java</h6>
+                </div>
+              </article>
+
+              <article className='experience__details'>
+                <div className='experience__details-div'>
+                  <BsCheck2Square className='experience__details-icon' />
                   <h6>Python</h6>
-                  <small className='text-light'> experienced</small>
                 </div>
-                </article>
+              </article>
+              <article className='experience__details'>
+                <div className='experience__details-div'>
+                  <BsCheck2Square className='experience__details-icon' />
+                  <h6>C#</h6>
+                </div>
+              </article>
 
               <article className='experience__details'>
-                <BsCheck2Square className='experience__details-icon' />
-                <div>
-                  <h6>Self Starter</h6>
-                  <small className='text-light'> experienced</small>
-                </div>
-                </article>
-
-              <article className='experience__details'>
-                <BsCheck2Square className='experience__details-icon' />
-                <div>
-                  <h6>Problem Solving</h6>
-                  <small className='text-light'> experienced</small>
+                <div className='experience__details-div'>
+                  <BsCheck2Square className='experience__details-icon' />
+                  <h6>AWS</h6>
                 </div>
               </article>
             </div>
