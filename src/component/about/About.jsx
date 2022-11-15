09877e7eef999539be20a1f0a6640973c9fb45ce
Eyouel <eyouelaya@gmail.com>
2022-11-15 21:10:22 +0000

    update about statement

--- src/component/about/About.jsx
+++ src/component/about/About.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './about.css'
-import ME from '../../assets/me-about.jpg' 
+import ME from '../../assets/me-about.jpg'
 
 export const About = () => {
   return (
@@ -9,14 +9,14 @@ export const About = () => {
       <h2>About Me</h2>
 
       <div className="container about__container">
-      <div className="about__me">
-        <div className="about__me-image">
-          <img src={ME} alt="About Image" />
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={ME} alt="About Image" />
+          </div>
         </div>
-      </div>
-      <div className="about__content">
-          <p>Currently a Market Specialist at META(FB), I support the Sub-Saharan Market by initiating and leading projects to ensure the safety of our communities. I work with multiple cross-functional teams within the Risk Prevention team. Through my experience in various roles throughout my career, I have developed strong problem-solving, critical thinking, software development, partner management, and leadership skills.</p>
-        <a href='#contact' className='btn btn-primary'>Let's Talk</a>
+        <div className="about__content">
+          <p>Currently a Market Specialist at META(FB), I support the Sub-Saharan Market by initiating and leading projects to ensure the safety of our communities. I also work on Web development projects on the side. Through my experience in various roles throughout my career, I have developed strong problem-solving, critical thinking, software development, partner management, and leadership skills.</p>
+          <a href='#contact' className='btn btn-primary'>Let's Talk</a>
         </div>
       </div>
     </section>
